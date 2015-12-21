6d45f6ec53e2e582b267cc00e56ab5b5d6625d3d
Chen <neil_chen_0918@live.cn>
2015-12-21 15:22:10 +0800

    fix csproj

--- src/IpSync.ServerPy.Wrapper/Program.cs
+++ src/IpSync.ServerPy.Wrapper/Program.cs
@@ -17,7 +17,7 @@ namespace IpSync.ServerPy.Wrapper
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new IpsyncService()
             };
             ServiceBase.Run(ServicesToRun);
         }
